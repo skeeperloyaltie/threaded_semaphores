5f5aa6cdcd408a26dc6a0640ffa6848a745c8f2a
skeeperloyaltie <gugod254@gmail.com>
2023-03-19 19:43:47 +0300

    chance_2

--- program_c.c
+++ program_c.c
@@ -5,29 +5,41 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <stdint.h>   // Add this line to include intptr_t type
+#include <stdint.h> // Add this line to include intptr_t type
 
 #define MAX_READERS 40
 
 int shared_resource = 0;
 sem_t mutex, writeblock;
 int numOfReaders, numOfWriters;
+void relaxandspendtime()
+{
+    int i;
+    for (i = 0; i < 250000000; i++)
+        i = i;
+}
 
-void *reader_thread(void *arg) {
-    intptr_t thread_id = (intptr_t) arg;   // Replace int with intptr_t
-    while (1) {
+void *reader_thread(void *arg)
+{
+    intptr_t thread_id = (intptr_t)arg;
+    while (1)
+    {
         sem_wait(&mutex);
         shared_resource++;
-        if (shared_resource == 1) {
+        if (shared_resource == 1)
+        {
             sem_wait(&writeblock);
         }
         sem_post(&mutex);
 
-        printf("Reader %ld: read shared resource as %d\n", thread_id, shared_resource);
+        printf("Reader %ld: Done 25M, last read value = %d\n", thread_id, shared_resource);
+
+        relaxandspendtime(); // Add this line
 
         sem_wait(&mutex);
         shared_resource--;
-        if (shared_resource == 0) {
+        if (shared_resource == 0)
+        {
             sem_post(&writeblock);
         }
         sem_post(&mutex);
@@ -36,25 +48,33 @@ void *reader_thread(void *arg) {
     }
 }
 
-void *writer_thread(void *arg) {
-    intptr_t thread_id = (intptr_t) arg;   // Replace int with intptr_t
-    while (1) {
+void *writer_thread(void *arg)
+{
+    intptr_t thread_id = (intptr_t)arg;
+    while (1)
+    {
         sem_wait(&writeblock);
         shared_resource++;
-        printf("Writer %ld: modified shared resource to %d\n", thread_id, shared_resource);
+        printf("Writer  Done %ld:M, last read value %d\n", thread_id, shared_resource);
         sem_post(&writeblock);
+
+        relaxandspendtime(); // Add this line
+
         sleep(1);
     }
 }
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
         printf("Usage: %s <number of readers> <number of writers>\n", argv[0]);
         return 1;
     }
 
     numOfReaders = atoi(argv[1]);
-    if (numOfReaders > MAX_READERS) {
+    if (numOfReaders > MAX_READERS)
+    {
         printf("Error: too many readers (max %d)\n", MAX_READERS);
         return 1;
     }
@@ -72,16 +92,18 @@ int main(int argc, char **argv) {
     pthread_attr_setdetachstate(&attr[0], PTHREAD_CREATE_JOINABLE);
 
     // Create reader threads
-    int k = (int) (numOfReaders/2);
-    for (intptr_t i = 0; i < k; i++) {
-        pthread_create(&readers[i], &attr[0], reader_thread, (void*) i);
+    int k = (int)(numOfReaders / 2);
+    for (intptr_t i = 0; i < k; i++)
+    {
+        pthread_create(&readers[i], &attr[0], reader_thread, (void *)i);
     }
 
     // Create writer thread
     pthread_create(&writers[0], &attr[0], writer_thread, NULL);
 
-    for (intptr_t i = k ; i < numOfReaders ; i++) {
-        pthread_create(&readers[i], &attr[0], reader_thread, (void*) i);
+    for (intptr_t i = k; i < numOfReaders; i++)
+    {
+        pthread_create(&readers[i], &attr[0], reader_thread, (void *)i);
     }
 
     // Wait for writer thread to finish
